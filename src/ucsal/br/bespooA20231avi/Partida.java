91b2862827955c4fcafab5bd31305d3f2c10ce20
Noah VNA <polnareffextratchicc@gmail.com>
2023-03-31 13:40:52 -0300

    primeiro commit

---
+++ src/ucsal/br/bespooA20231avi/Partida.java
@@ -0,0 +1,19 @@
+package ucsal.br.bespooA20231avi;
+
+public class Partida {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		for(int i=30;i>0;i--) {
+			System.out.println(i);
+			
+			Thread.sleep(1000);
+		}
+		System.out.println("Seu tempo acabou jogador!");
+	
+		
+		
+	}
+
+}
---
+++ src/ucsal/br/bespooA20231avi/Relogio.java
@@ -0,0 +1,10 @@
+package ucsal.br.bespooA20231avi;
+
+public class Relogio extends Thread{
+	 
+	@Override
+	public void run() {
+		
+	}
+
+}
